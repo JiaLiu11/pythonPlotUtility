1362cf2481c973456d7e136caaee9e02eda6ed6f
Jia Liu <liu.2053@osu.edu>
2015-09-03 14:28:59 -0400

    Only collect flow Qn vectors for merged hadrons.

--- ParticleReader.py
+++ ParticleReader.py
@@ -1026,12 +1026,12 @@ class ParticleReader(object):
                                            rap_type='pseudorapidity')
 
         self.collect_basic_particle_spectra()
-        self.collect_flow_Qn_vectors('charged')
+        # self.collect_flow_Qn_vectors('charged')
         for aPart in ['pion_p', 'kaon_p', 'proton']:
             self.collect_flow_Qn_vectors(aPart)
             self.collect_particle_meanPT(aPart)
 
-        #self.collect_flow_Qn_vectors_for_mergedHaron()
+        self.collect_flow_Qn_vectors_for_mergedHaron()
 
     def mergeAnalyzedDatabases(self, toDB, fromDB):
         """
